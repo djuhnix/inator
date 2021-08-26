02be615da75d26c160bb90c803e91d7022386db9
Germain OLEA-OYOUGOU <djuhngleen@gmail.com>
2021-08-26 16:42:49 +0200

    add morning command & new morning message

---
+++ commands/morning.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line no-unused-vars
+const { Message } = require('discord.js');
+const fs = require("fs");
+const { morningJobMessages } = JSON.parse(fs.readFileSync('config.json').toString());
+module.exports = {
+	name: 'morning',
+	usage: 'morning',
+	description: 'Donne au hasard une formule de salution utilisé par djuhnixinator.',
+	/**
+	 *
+	 * @param {Message} message
+	 * @param {String} args
+	 */
+	execute(message, args = '') {
+		let answer = morningJobMessages[Math.floor(Math.random() * morningJobMessages.length)];
+		message.channel.send(answer)
+			.then(() => {
+				message.delete({ timeout: 10 });
+			});
+	},
+};
