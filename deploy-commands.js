9bdb5238f731d4008cfc605bb78b694758596081
djuhnix <djuhngleen@gmail.com>
2022-09-13 17:59:25 +0200

    feat: add slash commands handler

---
+++ deploy-commands.js
@@ -0,0 +1,39 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+const { BOT_TOKEN, GUILD_ID, CLIENT_ID } = process.env;
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'slash-commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands })
+	.then(data => console.log(`Successfully registered ${data.length} application commands.`))
+	.catch(console.error);
+
+module.exports = {
+	async refreshCommands() {
+		try {
+			console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+			const data = await rest.put(
+				Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+				{ body: commands },
+			);
+
+			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		}
+		catch (error) {
+			console.error(error);
+		}
+	},
+};
