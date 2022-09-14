8d1443c3cdc086164c1426166a7302cf1f7c04d2
djuhnix <djuhngleen@gmail.com>
2022-09-14 16:09:38 +0200

    fix: check for message component in interaction

--- slash-commands/game.js
+++ slash-commands/game.js
@@ -5,16 +5,22 @@ module.exports = {
 		.setName('game')
 		.setDescription('Jouer à un jeu'),
 	async execute(interaction) {
-		const game = new TicTacToe({
-			message: interaction.message,
-			xEmoji: '❌',
-			oEmoji: '0️⃣',
-			xColor: 'PRIMARY',
-			oColor: 'PRIMARY',
-			opponent: interaction.member,
-			embedDescription: 'Tic Tac Toe',
-		});
-		await game.start();
+		if (interaction.isMessageComponent()) {
+			console.log('interaction is message component, trying to start game...');
+			const game = new TicTacToe({
+				message: interaction.message,
+				xEmoji: '❌',
+				oEmoji: '0️⃣',
+				xColor: 'PRIMARY',
+				oColor: 'PRIMARY',
+				opponent: interaction.member,
+				embedDescription: 'Tic Tac Toe',
+			});
+			await game.start();
+		}
+		else {
+			console.log('interaction is not message component');
+		}
 		await interaction.reply('Finish!');
 	},
 };
